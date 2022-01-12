1d9d27f4f99d0d0ac0b4b4667994e9c1cff83972
Ganesh Tiwari <Ganesht049@gmail.com>
2022-01-12 11:28:54 +0530

    added tailwindcss

---
+++ src/custom-components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className='flex'>
+            <div className='flex flex-col'>
+                <h1 className='poppins font-bold text-3xl'>Donaterra</h1>
+                <span>Crypto donations</span>
+            </div>
+        </nav>
+    )
+}
