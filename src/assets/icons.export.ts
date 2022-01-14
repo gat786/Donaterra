26845398b36c32e9225a90cef3f77033eb899798
Ganesh Tiwari <Ganesht049@gmail.com>
2022-01-14 20:20:29 +0530

    added icons

---
+++ src/assets/icons.export.ts
@@ -0,0 +1,21 @@
+import Annonymity from "assets/icons/annonymity-icon.png";
+import EasyToSetup from "assets/icons/easy-to-setup.png";
+import Global from "assets/icons/global-icon.png";
+import Secure from "assets/icons/secure-icon.png";
+import Speed from "assets/icons/speed-icon.png";
+import Transparent from "assets/icons/transparency-icon.png";
+
+const Features = {
+  Annonymity,
+  EasyToSetup,
+  Global,
+  Secure,
+  Speed,
+  Transparent,
+};
+
+const Icons = {
+  Features,
+};
+
+export default Icons;
---
+++ src/custom-components/FeatureCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function FeatureCard(featureTitle: string, featureIcon: string) {
+  return <div className="feature-card">
+
+  </div>;
+}
