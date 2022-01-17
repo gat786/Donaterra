938fdd620a37908ee4e06da0294c2c27a2c970aa
Ganesh Tiwari <Ganesht049@gmail.com>
2022-01-17 14:15:50 +0530

    added styling change to donation page

--- src/pages/DonationPage.tsx
+++ src/pages/DonationPage.tsx
@@ -47,33 +47,35 @@ export default function DonationPage() {
           </div>
         </section>
 
-        <section className="donation-details">
-          <h3 className="about-donation-heading">About the donation</h3>
-          <div className="donation-about">
-            What goes on in a mother’s mind when any time she visits the
-            hospital with her son, she is informed that his cancer has relapsed?
-            Having faced that situation thrice, I pray to God none of the
-            parents ever have the ill fortune to witness their own child
-            suffering from cancer from time and again.
-            <br />
-            My family's happiness lies in Abir. Him being the youngest member of
-            my family of four, all of us are accustomed to his joyous and
-            playful. When he became extremely sick the first time, he was scared
-            and sad. Yet he tried to cheer all of us up as much as he could.
-            2014-2017 In April 2014, I was given the worst news any mother could
-            receive.
-            <br />
-            It was the first time Abir was diagnosed with Leukemia. Abir was in
-            and out of hospital stays for three long years. He spent much of his
-            early childhood getting poked by needles, giving blood, sleeping in
-            hospital beds and receiving harsh chemotherapy.
-            <br />
-            He not only lost all his hair, his body weight also decreased
-            drastically. Finally, in June 2017, I finally received news my heart
-            was aching to hear. After 3 years of painful treatments, Abir had
-            finally beat cancer. He could finally live his life like a normal
-            child. However, fate had other plans.
-          </div>
+        <section className="donation-bottom">
+          <section className="donation-details">
+            <h3 className="about-donation-heading">About the donation</h3>
+            <div className="donation-about">
+              What goes on in a mother’s mind when any time she visits the
+              hospital with her son, she is informed that his cancer has
+              relapsed? Having faced that situation thrice, I pray to God none
+              of the parents ever have the ill fortune to witness their own
+              child suffering from cancer from time and again.
+              <br />
+              My family's happiness lies in Abir. Him being the youngest member
+              of my family of four, all of us are accustomed to his joyous and
+              playful. When he became extremely sick the first time, he was
+              scared and sad. Yet he tried to cheer all of us up as much as he
+              could. 2014-2017 In April 2014, I was given the worst news any
+              mother could receive.
+              <br />
+              It was the first time Abir was diagnosed with Leukemia. Abir was
+              in and out of hospital stays for three long years. He spent much
+              of his early childhood getting poked by needles, giving blood,
+              sleeping in hospital beds and receiving harsh chemotherapy.
+              <br />
+              He not only lost all his hair, his body weight also decreased
+              drastically. Finally, in June 2017, I finally received news my
+              heart was aching to hear. After 3 years of painful treatments,
+              Abir had finally beat cancer. He could finally live his life like
+              a normal child. However, fate had other plans.
+            </div>
+          </section>
         </section>
       </div>
     </>
